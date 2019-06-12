363f7b82802dad4a3a66986e45ba006426979f29
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-11 23:12:11 -0600

    add store and employee validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,7 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates_numericality_of :annual_revenue, :only_integer => true
 end
+
+puts Store.errors[:name]
